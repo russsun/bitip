1668514bae2866f1ca6bb7b7e52b91e1315aa243
hongyu sun <dr.sunhongyu@gmail.com>
2018-05-06 13:23:16 -0700

    Update BITIP logo and description

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -11,15 +11,15 @@ export default class Signin extends Component {
 
     return (
       <div className="panel-landing" id="section-1">
-        <h1 className="landing-heading">Publik</h1>
-        <p>Decentralized micro-blogging</p>
+        <h1 className="landing-heading">BITIP</h1>
+        <p>Decentralized Timestamp Verification System</p>
         <p className="lead">
           <button
             className="btn btn-primary btn-lg"
             id="signin-button"
             onClick={ handleSignIn.bind(this) }
           >
-            Sign In with Blockstack
+            Sign In
           </button>
         </p>
       </div>

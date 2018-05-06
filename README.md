fdd7ec2e45577a0dca6c66ea6f05c898c357a036
hongyu sun <dr.sunhongyu@gmail.com>
2018-05-05 20:30:08 -0700

    Initial checkin for working demo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # bitip
+
+Please follow the instruction to setup the npm dev env
+https://blockstack.org/tutorials/multi-player-storage
+
+Also install react
+npm i -D uglifyjs-webpack-plugin
---
+++ src/components/Signin.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { isUserSignedIn } from 'blockstack';
+
+export default class Signin extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { handleSignIn } = this.props;
+
+    return (
+      <div className="panel-landing" id="section-1">
+        <h1 className="landing-heading">Publik</h1>
+        <p>Decentralized micro-blogging</p>
+        <p className="lead">
+          <button
+            className="btn btn-primary btn-lg"
+            id="signin-button"
+            onClick={ handleSignIn.bind(this) }
+          >
+            Sign In with Blockstack
+          </button>
+        </p>
+      </div>
+    );
+  }
+}
